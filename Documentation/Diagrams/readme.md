57006048672df9126f4e386b9b5141470d22e1c3
Noe Švanda <svanda.noe@gmail.com>
2024-04-11 18:48:24 +0200

    diagrams generation

---
+++ Documentation/Diagrams/readme.md
@@ -0,0 +1,8 @@
+# Diagrams
+
+This folder contains diagrams that are used in the documentation.
+
+Technologies used:
+
+- [Diagrams as Code](https://diagrams.mingrammer.com/)
+- [Graphviz](https://graphviz.org/)
---
+++ Documentation/Diagrams/stack-data_flow.py
@@ -0,0 +1,74 @@
+from diagrams import Cluster, Diagram
+from diagrams.onprem.logging import Loki
+from diagrams.onprem.tracing import Tempo
+from diagrams.onprem.database import Postgresql, Influxdb
+from diagrams.onprem.monitoring import Grafana, Prometheus
+from diagrams.onprem.network import Nginx
+from diagrams.onprem.queue import Rabbitmq
+from diagrams.programming.language import Csharp
+from diagrams.custom import Custom
+
+with Diagram(
+    "DTA Stack - Data Flow",
+    show=False,
+    direction="LR",
+    strict=True,
+    filename="Documentation/Diagrams/generated/stack-data-flow",
+    outformat="png",
+):
+    ingress = Nginx("Ingress")
+    rabbit = Rabbitmq("Message Broker")
+    database = Postgresql("PostgreSQL")
+    influx = Influxdb("InfluxDB")
+    k6 = Custom("Testing", "../logos/k6.png")
+
+    lgtm = Cluster("LGTM")
+    services = Cluster("Services")
+    aot = Cluster("AOT")
+    jit = Cluster("JIT")
+
+    otel = Custom("OTel", "../logos/otel.png")
+
+    with lgtm:
+        grafana = Grafana("Monitoring")
+        prom = Prometheus("Metrics")
+        tempo = Tempo("Tracing")
+        loki = Loki("Logging")
+
+        otel >> prom >> grafana
+        otel >> tempo >> grafana
+        otel >> loki >> grafana
+        k6 >> influx >> grafana
+
+    with aot:
+        fus_aot = Csharp("FUS")
+        srs_aot = Csharp("SRS")
+        bps_aot = Csharp("BPS")
+        eps_aot = Csharp("EPS")
+
+        eps_aot >> rabbit >> bps_aot
+        bps_aot >> fus_aot
+
+    with jit:
+        fus_jit = Csharp("FUS")
+        srs_jit = Csharp("SRS")
+        bps_jit = Csharp("BPS")
+        eps_jit = Csharp("EPS")
+
+        eps_jit >> rabbit >> bps_jit
+        bps_jit >> fus_jit
+
+    fus_aot >> database
+    fus_jit >> database
+
+    fus_aot >> otel
+    fus_jit >> otel
+    srs_aot >> otel
+    srs_jit >> otel
+    bps_aot >> otel
+    bps_jit >> otel
+    eps_aot >> otel
+    eps_jit >> otel
+
+    ingress >> grafana
+    k6 >> services
