e5e14f2ed1f1f347a1b74c6bb3a8c68e5bc75223
Noe Švanda <svanda.noe@gmail.com>
2024-05-05 23:04:51 +0200

    stack telemetry data diagram

--- Documentation/Diagrams/stack-data_flow.py
+++ Documentation/Diagrams/stack-data_flow.py
@@ -16,9 +16,6 @@ with Diagram(
     filename="Documentation/Diagrams/generated/stack-data-flow",
     outformat="png",
 ):
-    ingress = Nginx("Ingress")
-    rabbit = Rabbitmq("Message Broker")
-    database = Postgresql("PostgreSQL")
     influx = Influxdb("InfluxDB")
     k6 = Custom("Testing", "../logos/k6.png")
 
@@ -28,17 +25,21 @@ with Diagram(
     jit = Cluster("JIT")
 
     otel = Custom("OTel", "../logos/otel.png")
+    node = Prometheus("Node Exporter")
+    cadvisor = Custom("Cadvisor", "../logos/cadvisor.png")
 
     with lgtm:
         grafana = Grafana("Monitoring")
         prom = Prometheus("Metrics")
         tempo = Tempo("Tracing")
         loki = Loki("Logging")
-
         otel >> prom >> grafana
         otel >> tempo >> grafana
         otel >> loki >> grafana
         k6 >> influx >> grafana
+
+        node >> prom
+        cadvisor >> prom
 
     with aot:
         fus_aot = Csharp("FUS")
@@ -46,7 +47,6 @@ with Diagram(
         bps_aot = Csharp("BPS")
         eps_aot = Csharp("EPS")
 
-        eps_aot >> rabbit >> bps_aot
         bps_aot >> fus_aot
 
     with jit:
@@ -55,11 +55,7 @@ with Diagram(
         bps_jit = Csharp("BPS")
         eps_jit = Csharp("EPS")
 
-        eps_jit >> rabbit >> bps_jit
         bps_jit >> fus_jit
-
-    fus_aot >> database
-    fus_jit >> database
 
     fus_aot >> otel
     fus_jit >> otel
@@ -70,5 +66,4 @@ with Diagram(
     eps_aot >> otel
     eps_jit >> otel
 
-    ingress >> grafana
     k6 >> services
